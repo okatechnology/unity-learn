24535113aca8ef4d379b0817cbfea5abdd70a65a
okatechnology <tonkotsu6218@gmail.com>
2023-05-08 11:21:16 +0900

    initial commit
    initial

---
+++ Assets/GridController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class GridController : MonoBehaviour
+{
+    public GameObject tilePrefab;
+    public int gridSize = 5;
+    public float tileSize = 1.0f;
+
+    private List<GameObject> tiles = new List<GameObject>();
+
+    void Start()
+    {
+        GenerateGrid();
+    }
+
+    void GenerateGrid()
+    {
+        float gridOffset = (gridSize - 1) * tileSize * 0.5f;
+
+        for (int y = 0; y < gridSize; y++)
+        {
+            for (int x = 0; x < gridSize; x++)
+            {
+                Vector3 tilePosition = new Vector3(x * tileSize - gridOffset, y * tileSize - gridOffset, 0);
+                GameObject newTile = Instantiate(tilePrefab, tilePosition, Quaternion.identity);
+                newTile.transform.SetParent(transform);
+                tiles.Add(newTile);
+            }
+        }
+    }
+}
---
+++ Assets/TileController.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class TileController : MonoBehaviour
+{
+    private TextMeshProUGUI numberText;
+    private int number = 0;
+    private static int nextNumber = 1;
+
+    void Start()
+    {
+        GameObject textObject = new GameObject("NumberText");
+        textObject.transform.SetParent(transform);
+        textObject.transform.localPosition = Vector3.zero;
+
+        RectTransform rectTransform = textObject.AddComponent<RectTransform>();
+        rectTransform.anchorMin = Vector2.zero;
+        rectTransform.anchorMax = Vector2.one;
+        rectTransform.sizeDelta = Vector2.zero;
+        rectTransform.localPosition = Vector3.zero;
+
+        numberText = textObject.AddComponent<TextMeshProUGUI>();
+        numberText.font = Resources.Load<TMP_FontAsset>("Fonts & Materials/LiberationSans SDF");
+        numberText.fontSize = 1;
+        numberText.alignment = TextAlignmentOptions.Center;
+        numberText.color = Color.black;
+        UpdateNumberText();
+    }
+
+    void UpdateNumberText()
+    {
+        numberText.text = number > 0 ? number.ToString() : "A";
+    }
+
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                CheckInput(touch.position);
+            }
+        }
+        Debug.Log("aaaa");
+    }
+
+    void OnMouseDown()
+    {
+        HandleClickOrTap();
+    }
+
+    private void CheckInput(Vector3 inputPosition)
+    {
+        Ray ray = Camera.main.ScreenPointToRay(inputPosition);
+        RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction);
+
+        if (hit && hit.transform == transform)
+        {
+            HandleClickOrTap();
+        }
+    }
+
+    private void HandleClickOrTap()
+    {
+        
+        if (number == 0)
+        {
+            number = nextNumber++;
+            UpdateNumberText();
+        }
+    }
+}
